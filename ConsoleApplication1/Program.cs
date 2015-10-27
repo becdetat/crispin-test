1438685aefc36566dedc144b2633c3d602eab034
Ben Scott <ben@belfryimages.com.au>
2015-10-28 07:07:32 +1000

    POC test for Crispin using the NuGet packages

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASP;
+using crispin;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new _test_cshtml();
+            var xrpt = test.TransformText();
+
+            // Convert to byte[] for streaming etc:
+            //var converter = new PdfReportConverter();
+            //var buffer = converter.ConvertToBuffer(xrpt, "Test report");
+            //Console.Write(buffer);
+
+            var reportingService = new ReportingService();
+            reportingService.OpenAsPdf(xrpt, "Test report");
+
+            Console.Read();
+        }
+    }
+}
